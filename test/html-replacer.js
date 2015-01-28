357affb17d4b9900f80bd8bb1a4d3b0e55f893a5
Steve Carratt <scarratt@simplifydigital.co.uk>
2015-01-28 15:53:04 +0000

    adding hmtl replacer class

---
+++ test/html-replacer.js
@@ -0,0 +1,17 @@
+/**
+ * Created by scarratt on 28/01/2015.
+ */
+
+var htmlReplace = require('../src/html-replacer');
+
+require("mocha");
+
+
+describe('html replacer', function() {
+
+	it('should return badger', function() {
+		var htmlReplacer = htmlReplace(configFile);
+
+		var localisedHtml = htmlReplace(hmtl);
+	});
+});
